c7ecc8b7f55479237b45238fb03f648be4531ccc
Lucian Kahn <luciankahn@gmail.com>
2015-04-09 20:14:30 -0400

    downcase on email addresses

---
+++ lib/test.rb
@@ -0,0 +1,9 @@
+require_relative 'tubular-faker.rb'
+
+puts "Name: " + TubularFaker.name
+puts "Company: " + TubularFaker.company
+puts "City: " + TubularFaker.city
+puts "Email: " + TubularFaker.email
+puts "URL: " + TubularFaker.url
+puts "Date: " + TubularFaker.date
+puts "Lingo " + TubularFaker.lingo
