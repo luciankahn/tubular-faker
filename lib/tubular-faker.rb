817b444e7b03aad4ed2b9668fa6fe1aaa27a7425
Lucian Kahn <luciankahn@gmail.com>
2015-04-09 20:35:37 -0400

    add some adjectives

--- lib/tubular-faker.rb
+++ lib/tubular-faker.rb
@@ -2,21 +2,19 @@ module TubularFaker
 
   #### OPTIONS
 
-  GENERAL = {
-    adjectives: ["Absolute", "Awesome", "Bogus", "Bomb", "Booyah", "Bummer", "Busted", "Chillin'", "Cowabunga", "Creepy", "Cyber", "Dope", "Exactamundo", "Extreme", "Far Out", "Fly", "Gnarly", "Grunge", "Hasta la Vista", "Hella", "Kewl", "Mega", "Mondo", "Nuclear", "Phat", "Rad", "Radical", "Rockin' Rappin'", "Shady", "Spooky", "Tight", "Total", "Tubular", "Tubuloso", "Whatever", "Wicked", "Wild and Crazy", "l33t"],
-    numbers: ["1001", "1337", "180", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2K", "3000", "311", "360", "64", "666", "667", "69", "94", "95", "96", "97", "98", "99"],
-    years: ["1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999"]
-  }
-  NAMES = {
-    first_names: ["2Pac", "Alanis", "Ani", "Axl", "Balki", "Bart", "Beavis", "Beck", "Billy", "Bjork", "Blossom", "Bono", "Busta", "Butthead", "Calista", "Chandler", "Clarissa", "Coolio", "Courtney", "Dylan", "Eddie", "Frasier", "Gavin", "Geraldo", "Gwen", "Heather", "Hootie", "Ice", "Janet", "Jessica", "Kevin", "Krist", "Kurt", "Lars", "Lauryn", "Luke", "MacGyver", "Madonna", "Mariah", "Mark-Paul", "Melissa Joan", "Missy", "Neve", "Prince", "Regis", "River", "Roseanne", "Sarah Michelle", "Screech", "Scully", "Seal", "Sheryl", "Sinead", "Snoop", "The Notorious", "Thom", "Tiffani", "Tori", "Tre", "Trent", "Uma", "Vanilla", "Whitney", "Zack"],
-    last_names: ["Amos", "Aniston", "Applegate", "B.I.G.", "Blige", "Bono", "Brandis", "Carey", "Carrey", "Clooney", "Cobain", "Cool J.", "Corgan", "Cosby", "Crow", "Cube", "DiFranco", "Dogg", "Doherty", "Dre", "Duchovny", "Elfman", "Flockhart", "Gellar", "Gosselaar", "Grohl", "Houston", "Ice", "Jackson", "Kiedis", "Kramer", "Latifah", "Lawless", "Lewinsky", "Locklear", "Loder", "Love", "Love Hewitt", "Manson", "Morissette", "Novaselic", "Perry", "Phair", "Phoenix", "Pickles", "Priestley", "Reznor", "Rhymes", "Rose", "Rossdale", "Rza", "Seinfeld", "Shakur", "Simpson", "Soprano", "Sorbo", "Stamos", "Stefani", "Taylor Thomas", "Ulrich", "Urkel", "Vedder", "Weiland"]
-  }
   ADDRESSES = {
     city_prefix: ["East", "Lake", "New", "North", "Port", "South", "West"],
     city_suffix: ["berg", "chester", "furt", "haven", "land", "shire", "town", "view", "ville"]
   }
+
   COMPANIES = {
-    products: ["3D", "Applets", "Arcade", "BBS", "Babies", "Beanies", "Beans", "Beepers", "Body Splash", "Boots", "Cards", "Cereal", "Dot-Com", "Dudes", "Dudettes", "Flannel", "Frames", "Headphones", "Hot Links", "IIGS", "Jewels", "Juice", "Net", "Netiquette", "Pogs", "Screensavers", "Scrunchies", "Shimmer", "Snax", "Sneakers", "Soakers", "Soda", "Stickers", "Superhighway", "Tapes", "Trolls", "Virtual Reality"]
+    products: ["3D", "Applets", "Arcade", "BBS", "Babies", "Beanies", "Beans", "Beepers", "Bellybutton Rings", "Body Splash", "Boots", "Cards", "Cereal", "Dot-Com", "Dudes", "Dudettes", "Flannel", "Frames", "Hair Dye", "Headphones", "Hot Links", "IIGS", "Jewels", "Juice", "Net", "Netiquette", "Pogs", "Screensavers", "Scrunchies", "Shimmer", "Snax", "Sneakers", "Soakers", "Soda", "Stickers", "Superhighway", "Tapes", "Trolls", "Virtual Reality"]
+  }
+
+  GENERAL = {
+    adjectives: ["Absolute", "Awesome", "Biodegradable", "Bogus", "Bomb", "Booyah", "Bummer", "Busted", "Chillin'", "Cowabunga", "Creepy", "Cyber", "Dope", "Emo", Exactamundo", "Extreme", "Far Out", "Fly", "Goth", "Gnarly", "Grunge", "Hasta la Vista", "Hella", "Kewl", "Mega", "Mondo", "Nuclear", "Phat", "Rad", "Radical", "Rockin' Rappin'", "Shady", "Spooky", "Tight", "Total", "Tubular", "Tubuloso", "Whatever", "Wiccan", "Wicked", "Wild and Crazy", "l33t"],
+    numbers: ["1001", "1337", "180", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2K", "3000", "311", "360", "64", "666", "667", "69", "94", "95", "96", "97", "98", "99"],
+    years: ["1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999"]
   }
 
   INTERNET = {
@@ -24,8 +22,14 @@ module TubularFaker
     dot: [".com", ".net"],
     pages: ["applet", "chatroom", "cyber", "fanpage", "guestbook", "homepage", "hotlinks", "index", "website"]
   }
+
   LINGO = {
     catchphrases: ["All that and a bag of chips.", "Allllllrighty then.", "As if!", "Bling bling!", "D'oh!", "Did I do that?", "Don't go there!", "Doy hickey.", "Eat my shorts!", "How's it hangin'?", "I am the great cornholio!", "I see dead people.", "It's all good.", "It's da bomb!", "It's not a tumor.", "Kewl beans.", "L8r sk8r!", "Let's bounce!", "Mad props.", "NOT!", "No duh.", "No soup for you!", "Oh my god, they killed Kenny!", "Oh snap!", "PSYCH!", "Peace out, cub scout!", "Run Forrest, run!", "Scha-wing!", "Score!", "So funny I forgot to laugh.", "So is your FACE.", "Take a chill pill!", "Talk to the hand.", "Then why don't you marry it?", "There's no crying in baseball.", "What up, dawg?", "What's the dillio?", "Yadda yadda yadda.", "Yoink!", "You go girl!", "You got it, dude.", "Your mom."]
+  }
+
+  NAMES = {
+    first_names: ["2Pac", "Alanis", "Ani", "Axl", "Balki", "Bart", "Beavis", "Beck", "Billy", "Bjork", "Blossom", "Bono", "Busta", "Butthead", "Calista", "Chandler", "Clarissa", "Coolio", "Courtney", "Dylan", "Eddie", "Frasier", "Gavin", "Geraldo", "Gwen", "Heather", "Hootie", "Ice", "Janet", "Jessica", "Kevin", "Krist", "Kurt", "Lars", "Lauryn", "Luke", "MacGyver", "Madonna", "Mariah", "Mark-Paul", "Melissa Joan", "Missy", "Neve", "Prince", "Regis", "River", "Roseanne", "Sarah Michelle", "Screech", "Scully", "Seal", "Sheryl", "Sinead", "Snoop", "The Notorious", "Thom", "Tiffani", "Tori", "Tre", "Trent", "Uma", "Vanilla", "Whitney", "Yakko", "Zack"],
+    last_names: ["Amos", "Aniston", "Applegate", "B.I.G.", "Blige", "Boitano", "Bono", "Brandis", "Carey", "Carrey", "Clooney", "Cobain", "Cool J.", "Corgan", "Cosby", "Crow", "Cube", "DiFranco", "Dogg", "Doherty", "Dre", "Duchovny", "Elfman", "Flockhart", "Gellar", "Gosselaar", "Grohl", "Houston", "Ice", "Jackson", "Kiedis", "Kramer", "Latifah", "Lawless", "Lewinsky", "Locklear", "Loder", "Love", "Love Hewitt", "Manson", "Morissette", "Novaselic", "Perry", "Phair", "Phoenix", "Pickles", "Priestley", "Reznor", "Rhymes", "Rose", "Rossdale", "Rza", "Seinfeld", "Shakur", "Simpson", "Soprano", "Sorbo", "Stamos", "Stefani", "Taylor Thomas", "Ulrich", "Urkel", "Vedder", "Weiland"]
   }
 
   #### METHODS
